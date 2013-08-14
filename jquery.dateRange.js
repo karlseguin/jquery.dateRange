4ab98b1c831dd62df74b7726c31d3066eb1ec180
Karl Seguin <karl@openmymind.net>
2013-08-14 22:53:49 +0800

    removed extra spaces

--- jquery.dateRange.js
+++ jquery.dateRange.js
@@ -6,16 +6,16 @@
       var months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
       var abbreviations = new Array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec');
       var daySelector = 'td:not(.m):not(:empty)';
-      return this.each(function() 
+      return this.each(function()
       {
          if (this.dateRange) { return false; }
-      
+
          var $input = $(this);
          var $container, selecting, selected;
-         var self = 
+         var self =
          {
-            initialize: function() 
-            {               
+            initialize: function()
+            {
                $container = self.initializeContainer().hide();
                $prev = $container.delegate('td.prev', 'click', self.loadPrevious);
                $next = $container.delegate('td.next', 'click', self.loadNext);
@@ -25,7 +25,7 @@
                $container.append(self.buildMonth(prev));
                $container.append(self.buildMonth(now));
                self.fixNav();
-               
+
                $input.click(function()
                {
                   self.show();
@@ -34,15 +34,15 @@
                {
                  if (e.keyCode == 13) { self.entered(); }
                });
-               
+
                $(document).keydown(function(e)
                {
                   if (e.keyCode == 27) { self.hide(); }
                }).click(self.hide);
-               
+
                $container.delegate(daySelector, 'click', self.clicked);
                $container.click(function(){return false;});
-               
+
                if (opts.startWith != null)
                {
                   selected = opts.startWith;
@@ -53,11 +53,11 @@
             {
               var values = $input.val().split('-');
               if (values.length != 2) { return false; }
-              
+
               var from = self.parseDate(values[0].replace(/^\s*/, '').replace(/\s*$/, ''));
               var to = self.parseDate(values[1].replace(/^\s*/, '').replace(/\s*$/, ''));
               if (from == null || to == null) { return false; }
-              
+
               selected = [from, to];
               self.rangeSelected();
               return false;
@@ -87,7 +87,7 @@
                }
             },
             rangeSelected: function()
-            {            
+            {
                if (selected[0] > selected[1])
                {
                   var x = selected[0];
@@ -98,27 +98,27 @@
                self.highlight($container.find('table:first'));
                self.highlight($container.find('table:last'));
                self.hide();
-               if (opts.selected != null) { opts.selected(selected); } 
+               if (opts.selected != null) { opts.selected(selected); }
             },
             highlight: function($table)
             {
                if (selected == null || selected.length != 2) { return; }
-               
+
                $table.find('td.highlight').removeClass('highlight');
                var startDate = $table.data('date');
                var endDate = new Date(startDate.getFullYear(), startDate.getMonth()+1, 0);
                if (startDate > selected[1] || endDate < selected[0]) { return; }
-               
+
                var $tds = $table.find(daySelector);
                var start = selected[0].getMonth() < startDate.getMonth() || selected[0].getFullYear() < startDate.getFullYear() ? 0 : selected[0].getDate()-1;
                var end = selected[1].getMonth() > endDate.getMonth() || selected[1].getFullYear() > endDate.getFullYear() ? $tds.length : selected[1].getDate();
                for(var i = start; i < end; ++i)
                {
                   $tds.get(i).className = 'highlight';
-               }               
+               }
             },
             loadPrevious: function()
-            {               
+            {
                $container.children('table:eq(1)').remove();
                var $remaining = $container.children('table')
                var date = $remaining.data('date');
@@ -126,11 +126,11 @@
                self.fixNav();
             },
             loadNext: function()
-            {               
+            {
                $container.children('table:eq(0)').remove();
                var $remaining = $container.children('table')
                var date = $remaining.data('date');
-               $remaining.after(self.buildMonth(new Date(date.getFullYear(), date.getMonth()+1, 1)));            
+               $remaining.after(self.buildMonth(new Date(date.getFullYear(), date.getMonth()+1, 1)));
                self.fixNav();
             },
             initializeContainer: function()
@@ -147,7 +147,7 @@
                var totalDays = last.getDate();
                var weeks = Math.ceil((totalDays + firstDay) / 7);
                var table = document.createElement('table');
-               
+
                for (var i = 0, count = 1; i < weeks; ++i)
                {
                   var row = table.insertRow(-1);
@@ -163,10 +163,10 @@
                self.insertHeader(date, table);
                var $table = $(table).data('date', date);
                self.highlight($table);
-               
+
                if (opts.minimumDate && opts.minimumDate >= first) { self.prev().hide() } else { self.prev().show(); }
                if (opts.maximumDate && opts.maximumDate <= last) { self.next().hide() } else { self.next().show(); }
-               
+
                return $table;
             },
             format: function(date)
@@ -178,12 +178,12 @@
               var header = table.insertRow(0);
               var cell = header.insertCell(-1);
               cell.className = 'm nav';
-              
+
               cell = header.insertCell(1);
               cell.innerHTML = months[date.getMonth()] + ' ' + date.getFullYear();
               cell.className = 'm'; //very stupid IE (all versions) fix
               cell.colSpan = 5;
-              
+
               cell = header.insertCell(2);
               cell.className = 'm nav';
             },
@@ -195,12 +195,12 @@
             {
               return $container.find('div.next');
             },
-            fixNav: function() 
+            fixNav: function()
             {
               var $first = $container.find('table:first tr:first');
               $first.find('td:first').addClass('prev nav').html('&lsaquo;')
               $first.find('td:last').removeClass('next nav').html('');
-          
+
               var $second = $container.find('table:last tr:first');
               $second.find('td:first').removeClass('prev nav').html('')
               $second.find('td:last').addClass('next nav').html('&rsaquo;');
